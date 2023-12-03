24cc05639e005c734f3483a31be304605d5145d3
LauraCallaghan <88290691+LauraCallaghan@users.noreply.github.com>
2023-12-03 20:01:59 +0000

    attempt 2

--- database.py
+++ database.py
@@ -38,6 +38,6 @@ def load_job_from_db(id):
 def add_application_to_db(job_id,data):
   with engine.connect() as conn:
     query = text(
-      "INSERT INTO applications (job_id, full_name, email, linkedin, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin, :education, :work_experience, :resume_url)")
-   
-    conn.execute(query, job_id=job_id,full_name=data['full_name'], email=data['email'], linkedin=data['linkedin'], education=data['education'], work_experience=data['work_experience'], resume_url=data['resume_url'])
+      f"INSERT INTO applications (job_id, full_name, email, linkedin, education, work_experience, resume_url) VALUES ({job_id}, {data['full_name']}, {data['email']}, {data['linkedin']}, {data['education']}, {data['work_experience']}, {data['resume_url']})")
+
+    conn.execute(query)
